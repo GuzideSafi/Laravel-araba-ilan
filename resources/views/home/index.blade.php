3d6b2e1fce936ae09038d785ccb4d6dae62e3637
GuzideSafi <safiguzide@gmail.com>
2020-12-13 20:15:40 +0300

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>First Laravel Page</title>
+</head>
+<body>
+
+<h1>Welcome to laravel</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/home2', function () {
+    return view('welcome');
+});
+Route::redirect('/anasayfa', '/home');
+Route::get('/', function () {
+    return view('home.index');
+});
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/test/{id}', [HomeController::class, 'test']);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
